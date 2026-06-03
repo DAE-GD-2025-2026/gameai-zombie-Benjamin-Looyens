51a2d330ca5e7b590d685ee6e25ee0f6d1de13b4
Benjamin-Looyens <benjamin.looyens@gmail.com>
2026-06-03 17:12:06 +0200

    feat: detect if stimuli is house

--- Plugins/GameFeatures/LooyensBenjaminZombie/Source/LooyensBenjaminZombieRuntime/StudentPerceptor.cpp
+++ Plugins/GameFeatures/LooyensBenjaminZombie/Source/LooyensBenjaminZombieRuntime/StudentPerceptor.cpp
@@ -1,8 +1,12 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "StudentPerceptor.h"
 
+// Base Project Includes
+#include <GameAI_Zombie/Survivor/SurvivorPawn.h>
+#include <GameAI_Zombie/Village/House/House.h>
+
+//#include "Utils/ItemsInclude.h"
 
 UStudentPerceptor::UStudentPerceptor()
 {
@@ -16,11 +20,24 @@ void UStudentPerceptor::BeginPlay()
 	if (auto PerceptionComp = GetOwner()->GetComponentByClass<UAIPerceptionComponent>())
 	{
 		PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &UStudentPerceptor::OnPerceptionUpdated);
+		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Green, FString::Printf(TEXT("Perception Setup Success")));
+	}
+	else {
+		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("Perception Setup Failure")));
 	}
 }
 
 void UStudentPerceptor::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
-	FString::Printf(TEXT("Saw Something!")));
+	GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Yellow, FString::Printf(TEXT("Saw Something!")));
+
+	if (AHouse* pHouse = Cast<AHouse>(Actor)) {
+		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Yellow, FString::Printf(TEXT("Saw a House!")));
+
+		if (ASurvivorPawn* pSurvivor = dynamic_cast<ASurvivorPawn*>(GetOwner())) {
+			const double dist = FVector::Distance(pSurvivor->GetActorLocation(), pHouse->GetActorLocation());
+
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, FString::Printf(TEXT("Distance between house and survivor: %f"), dist));
+		}
+	}
 }
---
+++ Plugins/GameFeatures/LooyensBenjaminZombie/Source/LooyensBenjaminZombieRuntime/Utils/ItemsInclude.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include <GameAI_Zombie/Items/Food.h>
+#include <GameAI_Zombie/Items/Medkit.h>
+#include <GameAI_Zombie/Items/Pistol.h>
+#include <GameAI_Zombie/Items/Shotgun.h>
+#include <GameAI_Zombie/Items/ItemType.h>
