745368893ae121d85404e5806ae280f465f1789e
Benjamin-Looyens <benjamin.looyens@gmail.com>
2026-06-05 17:57:57 +0200

    chore: split survivor utils in .h and .cpp files

---
+++ Plugins/GameFeatures/LooyensBenjaminZombie/Source/LooyensBenjaminZombieRuntime/Private/SurvivorUtils.cpp
@@ -0,0 +1,60 @@
+#include "SurvivorUtils.h"
+#include <GameAI_Zombie/Survivor/SurvivorPawn.h>
+#include <GameAI_Zombie/Common/HealthComponent.h>
+#include <GameAI_Zombie/Common/InventoryComponent.h>
+#include <GameAI_Zombie/Common/StaminaComponent.h>
+#include <GameAI_Zombie/Village/House/House.h>
+
+namespace SurvivorUtils {
+	int GetNumberOfFreeSlots(UInventoryComponent* pInventory)
+	{
+		if (pInventory) {
+			const auto& pItems = pInventory->GetInventory();
+
+			return Algo::Accumulate(pItems, 0,
+				[](int accumulated, const ABaseItem* pItem) {
+					return accumulated + static_cast<int>(pItem == nullptr);
+				}
+			);
+		}
+
+		return 0;
+	}
+
+	int GetNumberOfOwnedItems(UInventoryComponent* pInventory)
+	{
+		if (pInventory) return pInventory->GetInventoryCapacity() - GetNumberOfFreeSlots(pInventory);
+
+		return 0;
+	}
+
+	float GetHealthPercent(UHealthComponent* pHealth)
+	{
+		if (!pHealth) return 0.0f;
+
+		return static_cast<float>(pHealth->GetHealth()) / static_cast<float>(pHealth->GetMaxHealth());
+	}
+
+	float GetStaminaPercent(UStaminaComponent* pStamina)
+	{
+		if (!pStamina) return 0.0f;
+
+		return static_cast<float>(pStamina->GetCurrentStamina()) / static_cast<float>(pStamina->GetMaxStamina());
+	}
+
+	FBox HouseBoundsToBox(const FHouseBounds& houseBounds)
+	{
+		return {
+			houseBounds.Origin - houseBounds.Extent,
+			houseBounds.Origin + houseBounds.Extent
+		};
+	}
+
+	bool IsSurvivorWithinHouse(ASurvivorPawn* pSurvivor, const FHouseBounds& houseBounds)
+	{
+		if (!pSurvivor) return false;
+
+		const FBox houseBox = SurvivorUtils::HouseBoundsToBox(houseBounds);
+		return houseBox.IsInside(pSurvivor->GetActorLocation());
+	}
+}
